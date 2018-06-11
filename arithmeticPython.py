eefe647c6e28680bfd5f2d9296b1b8380b85ba41
TorczerA <coolrohit1996@gmail.com>
2018-06-12 03:16:09 +0530

    To show basic arithmetic operations in Python

---
+++ arithmeticPython.py
@@ -0,0 +1,24 @@
+#Arithmetics in Python
+
+a = 100
+b = 200
+
+print("a = ",a)
+print("b = ",b)
+print("a+b =",a+b)
+print("a-b =",a-b)
+print("a*b =",a*b)
+print("a/b =",a/b)
+print("a%b =",a%b)
+
+c = a + b
+
+print("c = a + b =",c)
+
+print("a = a + b -> a += b")
+
+print("a = a - b -> a -= b")
+
+print("a = a * b -> a *= b")
+
+print("a = a / b -> a /= b")
